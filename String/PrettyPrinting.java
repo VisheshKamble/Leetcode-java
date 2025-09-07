1a5b30b5c535cd5d098afc0a8d75ea6023cf9a66
VisheshKamble <visheshy2k17@gmail.com>
2025-09-07 16:29:00 +0530

    More

--- String/PrettyPrinting.java
+++ String/PrettyPrinting.java
@@ -1,6 +1,17 @@
+
+import java.util.ArrayList;
+
 class PrettyPrinting {
     public static void main(String[] args) {
-        float a = 122.3445f;
-        System.out.printf("Formatted number is %.2f", a);
+    //if object toString() will be called 
+    //float a = 122.3445f;
+    //System.out.printf("Formatted number is %.2f", a);
+    // System.out.printf("PIE: %.2f", Math.PI );
+    //System.out.printf("My name is %s and i am %s" , "Vishesh" , "Cool");
+      System.out.println("Vishesh" + new ArrayList<>());
+      System.out.println("Vishesh" + new Integer(18));
+
+      //atleast 1 String is required
+      System.out.println(new Integer(18) + "" + new ArrayList<>() );
     }
 }
