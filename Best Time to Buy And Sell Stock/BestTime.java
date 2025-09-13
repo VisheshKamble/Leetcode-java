755eb14067e9396bd59b8be24867976cddb093e4
VisheshKamble <visheshy2k17@gmail.com>
2025-09-13 23:45:04 +0530

    Best Time to buy and sell stock

---
+++ Best Time to Buy And Sell Stock/BestTime.java
@@ -0,0 +1,18 @@
+class BestTime {
+    public static void main(String[] args) {
+        int[] prices = {7,6,5,4,3,2,1};
+        System.out.println(bestTime(prices));
+    }
+    static int bestTime (int[] prices){
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0 ;
+        for(int price : prices){
+            if(price < minPrice){
+                minPrice = price ;
+            }else{
+                maxProfit = Math.max(maxProfit, price - minPrice);
+            }
+        }
+        return maxProfit;
+    }
+}
